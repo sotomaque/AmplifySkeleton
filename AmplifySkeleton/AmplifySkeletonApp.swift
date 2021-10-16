4d84e89518b6f5b7f031aae97e4f39f74b52e216
sotomaque <sotomaque@gmail.com>
2021-10-15 21:33:51 -0700

    Initial Commit

---
+++ AmplifySkeleton/AmplifySkeletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmplifySkeletonApp.swift
+//  AmplifySkeleton
+//
+//  Created by Enrique Sotomayor on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct AmplifySkeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
